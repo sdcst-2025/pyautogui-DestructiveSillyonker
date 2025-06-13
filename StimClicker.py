e7a6b9f840111f68d9a22773114c04e850671db8
DestructiveSillyonker <johnr636@deltalearns.ca>
2025-06-13 11:35:09 -0700

    sas

--- StimClicker.py
+++ StimClicker.py
@@ -11,6 +11,13 @@ Ux2 = 644
 Ux3 = 744
 
 Uy = 560
+
+Sx = 675
+Sy = 695
+Sy2 = 770
+
+Ly = 230
+
 time.sleep(1)
 
 keyboard.press_and_release('alt+tab')
@@ -18,27 +25,36 @@ keyboard.press_and_release('alt+tab')
 time.sleep(1)
 
 
-try:
-    while True:
-        for i in range(100):
-            pyautogui.moveTo(Cx, Cy)  
-            pyautogui.click()
-            
 
-        for i in range(10):
-            pyautogui.moveTo(Ux1, Uy)
-            pyautogui.click()
+while True:
+
+    print("Stimulating...")
+    pyautogui.moveTo(Cx, Cy)    
+    for i in range(100):  
+        pyautogui.click()
+        
+    print("Upgrading...")
+    pyautogui.moveTo(Ux1, Uy)
+    for i in range(10):
+        pyautogui.click()
               
-
-        for i in range(10):
-            pyautogui.moveTo(Ux2, Uy)
-            pyautogui.click()
+    print("Upgrading...")
+    pyautogui.moveTo(Ux2, Uy)
+    for i in range(10):
+        pyautogui.click()
              
+    print("Upgrading...")
+    pyautogui.moveTo(Ux3, Uy)
+    for i in range(10):
+        pyautogui.click()
 
-        for i in range(10):
-            pyautogui.moveTo(Ux3, Uy)
-            pyautogui.click()
+    print("Squishing...")
+    for i in range(3):
+        pyautogui.moveTo(Sx, Sy)
+        pyautogui.click()
+        pyautogui.moveTo(Sx, Sy2)
+        pyautogui.click()
              
+    pyautogui.moveTo(Ux3, Ly)
+    pyautogui.click()
 
-except:
-    keyboard.wait('esc')
